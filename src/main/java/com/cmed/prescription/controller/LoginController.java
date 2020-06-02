51ee7204c6dbea28b85b19f979af2c4a2f166a5c
fahimferoje <fabrahamjohn@gmail.com>
2020-06-02 20:38:55 +0600

    added edit and prescription view support

--- src/main/java/com/cmed/prescription/controller/LoginController.java
+++ src/main/java/com/cmed/prescription/controller/LoginController.java
@@ -1,13 +1,19 @@
 package com.cmed.prescription.controller;
 
+import com.cmed.prescription.service.PrescriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    PrescriptionService prescriptionService;
 
     @RequestMapping(value="/login", method = RequestMethod.GET)
     public ModelAndView login(){
@@ -17,7 +23,20 @@ public class LoginController {
     }
 
     @GetMapping("/prescription")
-    public String viewPrescription() {
-        return "prescription";
+    public ModelAndView viewPrescription() {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("prescriptions", prescriptionService.getAllPrescriptions());
+        modelAndView.setViewName("prescription");
+
+        return modelAndView;
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView editPrescription(@PathVariable("id") Long presId) {
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        return modelAndView;
     }
 }
