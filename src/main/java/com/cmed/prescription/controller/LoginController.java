14c4ceb3180cfd5a9d73a557d25f966f416a9cf4
fahimferoje <fabrahamjohn@gmail.com>
2020-06-02 19:08:56 +0600

    added login controller

--- src/main/java/com/cmed/prescription/controller/LoginController.java
+++ src/main/java/com/cmed/prescription/controller/LoginController.java
@@ -2,13 +2,18 @@ package com.cmed.prescription.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/")
-    public String viewLogin2() {
-        return "login";
+    @RequestMapping(value="/login", method = RequestMethod.GET)
+    public ModelAndView login(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
     }
 
     @GetMapping("/prescription")
