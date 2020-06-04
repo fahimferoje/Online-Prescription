a2a5327c3babe0347de32c5690c2bf7e11c05a77
fahimferoje <fabrahamjohn@gmail.com>
2020-06-05 01:32:51 +0600

    consuming api

--- src/main/resources/static/api-consume.js
+++ src/main/resources/static/api-consume.js
@@ -1,30 +1,52 @@
 $(document).ready(function () {
-    // $.get("https://rxnav.nlm.nih.gov/REST/interaction/interaction.json?rxcui=341248"
-    //     , function(data, status) {
-    //
-    //     console.log(data);
-    // });
+    $.get("https://rxnav.nlm.nih.gov/REST/interaction/interaction.json?rxcui=341248"
+        , function (data, status) {
 
-    const data = {
-        "nlmDisclaimer": "It is not the intention of NLM to provide specific medical advice, but rather to provide users with information to better understand their health and their medications. NLM urges you to consult with a qualified physician for advice about medications.",
-        "userInput": {
-            "sources": [
-                ""
-            ],
-            "rxcui": "341248"
-        }
-    };
+            const { interactionTypeGroup } = data;
+            const { interactionType } = interactionTypeGroup[0];
+            const { interactionPair } = interactionType[0];
 
-        // <tr>
-        //     <th scope="col">#</th>
-        //     <th scope="col">First</th>
-        //     <th scope="col">Last</th>
-        //     <th scope="col">Handle</th>
-        // </tr>
+            for (const singleIntegration of interactionPair) {
 
-    let tableHeaders = `<tr>  </tr>`
+                const {interactionConcept, severity = '', description = ''} = singleIntegration;
 
+                let header = `<tr><td scope="col" colspan="3"><b>Min Concept Item</b></td>
+                       <td colspan="3"><b>Source Concept Item</b></td>
+                       <td><b>Severity</b></td>
+                       <td><b>Description</b></td></tr>`;
 
+                $("#myTable").append(header);
 
+                header = `<tr><td><b>rxcui</b></td>
+                       <td><b>name</b></td>
+                       <td><b>tty</b></td>
+                       <td><b>id</b></td>
+                       <td><b>name</b></td>
+                       <td><b>url</b></td>
+                       <td>${severity}</td>
+                       <td>${description}</td></tr>`;
+                $("#myTable").append(header);
+
+                for (const interact of interactionConcept) {
+                    const {
+                        minConceptItem: {rxcui, name: minName, tty}
+                        , sourceConceptItem: {id, name: srcName, url}
+                    } = interact;
+
+                    const data = `<tr>
+                        <td>${rxcui}</td>
+                        <td>${minName}</td>
+                        <td>${tty}</td>
+                        <td>${id}</td>
+                        <td>${srcName}</td>
+                        <td>${url}</td>
+                    </tr>`;
+
+                    $("#myTable").append(data);
+                }
+
+            }
+
+        });
 });
 
