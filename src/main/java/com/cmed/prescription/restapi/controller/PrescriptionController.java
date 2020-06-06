1dd1ffd25a345475a309a1b02b59dee5ed27ccb6
fahimferoje <fabrahamjohn@gmail.com>
2020-06-06 18:15:54 +0600

    cleanup

--- src/main/java/com/cmed/prescription/restapi/controller/PrescriptionController.java
+++ src/main/java/com/cmed/prescription/restapi/controller/PrescriptionController.java
@@ -3,12 +3,10 @@ package com.cmed.prescription.restapi.controller;
 import com.cmed.prescription.web.service.PrescriptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/prescription")
-@Validated
 public class PrescriptionController {
 
     @Autowired
