debef5dd1cc9051e5acb056be4cf850ef906b4c8
fahimferoje <fabrahamjohn@gmail.com>
2020-06-06 14:11:09 +0600

    added custom authentication provider

---
+++ src/main/java/com/cmed/prescription/restapi/config/CustomAuthenticationManager.java
@@ -0,0 +1,57 @@
+package com.cmed.prescription.restapi.config;
+
+import com.cmed.prescription.web.service.JwtUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthenticationManager implements AuthenticationProvider {
+
+    @Autowired
+    private JwtUserDetailsService userDetailsService;
+
+    //@Autowired
+    //private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        UserDetails userDetails = userDetailsService
+                .loadUserByUsername((String) authentication.getPrincipal());
+
+        BCryptPasswordEncoder b = new BCryptPasswordEncoder();
+
+        boolean matched = b.matches((String) authentication.getCredentials()
+                , userDetails.getPassword());
+
+        if (!matched) {
+            throw new PasswordNotMatchException("Credentials does not match");
+        }
+
+        String token = jwtTokenUtil.generateToken(userDetails);
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken((String) authentication.getPrincipal(), token
+                        , userDetails.getAuthorities());
+
+        SecurityContextHolder.getContext()
+                .setAuthentication(usernamePasswordAuthenticationToken);
+
+        return usernamePasswordAuthenticationToken;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
